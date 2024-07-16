dc74e7f7126458d552f66c7e8dd9a129c32fea75
garold664 <garold664@gmail.com>
2024-07-16 07:48:48 +0300

    adds env var

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
-app.get('/', (req, res) => res.send('Express on Vercel'));
+const secret = process.env.SECRET;
+app.get('/', (req, res) => res.send(`Secret is ${secret}`));
 app.listen(3000, () => console.log('Server ready on port 3000.'));
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
-app.get('/', (req: Request, res: Response) => res.send('Express on Vercel'));
+const secret = process.env.SECRET;
+
+app.get('/', (req: Request, res: Response) => res.send(`Secret is ${secret}`));
 
 app.listen(3000, () => console.log('Server ready on port 3000.'));
