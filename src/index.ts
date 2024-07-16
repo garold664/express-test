b23457d446d4947c49e670092b89d6551a11b8df
garold664 <garold664@gmail.com>
2024-07-16 07:37:51 +0300

    adds strict mode

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 const app = express();
 
-app.get('/', (req, res) => res.send('Express on Vercel'));
+app.get('/', (req: Request, res: Response) => res.send('Express on Vercel'));
 
 app.listen(3000, () => console.log('Server ready on port 3000.'));
